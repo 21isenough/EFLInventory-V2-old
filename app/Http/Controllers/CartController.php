a9c8e9c2fa4de169f1c577ad99c0b0306f25b774
Ruben Waterman <rubenwaterman@me.com>
2019-11-09 22:51:27 +0000

    forgot semicolon

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -244,7 +244,7 @@ class CartController extends Controller {
         /* Information for the receipt */
         $allItems = array(
             foreach ($items as $item){
-                new item($item[name], $item[price])
+                new item($item[name], $item[price]);
             }
         );
         $subtotal = new item('Subtotal', '12.95');

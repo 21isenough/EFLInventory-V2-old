3eadccec0e2ce146a009833b51266437d9f036fd
Ruben Waterman <rubenwaterman@me.com>
2019-11-09 21:58:44 +0000

    change items

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -258,6 +258,30 @@ class CartController extends Controller {
         $printer -> setEmphasis(false);
 
         foreach ($items as $item) {
+            private $name;
+            private $price;
+            private $dollarSign;
+
+            public function __construct($name = '', $price = '', $dollarSign = false)
+            {
+                $this -> name = $name;
+                $this -> price = $price;
+                $this -> dollarSign = $dollarSign;
+            }
+            
+            public function __toString()
+            {
+                $rightCols = 10;
+                $leftCols = 38;
+                if ($this -> dollarSign) {
+                    $leftCols = $leftCols / 2 - $rightCols / 2;
+                }
+                $left = str_pad($this -> name, $leftCols) ;
+                
+                $sign = ($this -> dollarSign ? '$ ' : '');
+                $right = str_pad($sign . $this -> price, $rightCols, ' ', STR_PAD_LEFT);
+                return "$left$right\n";
+            }
             $printer -> text($item);
         }
         $printer -> setEmphasis(true);

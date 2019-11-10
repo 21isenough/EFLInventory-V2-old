9e21ba242ed7bb6581b704112a3fbbd35b6cf044
Ruben Waterman <rubenwaterman@me.com>
2019-11-10 00:26:31 +0000

    try with foreach loop

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -252,7 +252,9 @@ class CartController extends Controller {
 
         /* Information for the receipt */
         $allItems = array(
-            new item($item['name'], $item['price'])
+            foreach ($items as $item) {
+                new item($item['name'], $item['price'])
+            }            
         );
         $subtotal = new item('Subtotal', '12.95');
         $tax = new item('A local tax', '1.30');

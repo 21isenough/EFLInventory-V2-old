36de277e13a7b58326a185b15ea0a3a0bffa9f09
Ruben Waterman <rubenwaterman@me.com>
2019-11-10 00:02:10 +0000

    try with array.push

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -252,7 +252,7 @@ class CartController extends Controller {
         /* Information for the receipt */
         $allItems = array();
         foreach ($items as $item){
-            $allItems[$items] .= "{$item->name}, {$item->price}";
+            allItems.push({$item->name}, {$item->price});
         };
         $subtotal = new item('Subtotal', '12.95');
         $tax = new item('A local tax', '1.30');
